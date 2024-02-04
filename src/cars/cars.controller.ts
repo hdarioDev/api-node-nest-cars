313ff1f58bb915776ee64f12dc92603e491d1414
hdariodev <hdariodev@gmail.com>
2024-02-04 17:13:14 -0400

    feat: get by id base

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
   @Get()
   getAllCars() {
     return 'All Cars';
+  }
+
+  @Get(':id')
+  GetCarById(@Param('id') id: string) {
+    console.log('param', id);
+    return `Car with id: ${id}`;
   }
 }
